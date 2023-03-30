261a295a1f6917986aac46868c4674dfd0ebc9f6
Aryeh Mischel <asmischel5@gmail.com>
2023-03-29 21:15:09 -0400

    changed cubemaps imgs from jpegs to webp

--- scripts/test.js
+++ scripts/test.js
@@ -2,11 +2,35 @@
     schema: {
     },
     init: function() {
-        let el = this.el;
+ /*       let el = this.el;
+        
+        console.log("watching")
 
-        el.addEventListener('componentinitialized', () => console.log(el.id + " componentinitialized"))
-        el.addEventListener('loaded', () => console.log(el.id + " loaded"))
-        el.addEventListener('object3dset', () => console.log(el.id + " object3dset"))
+        // Select the node that will be observed for mutations
+        const targetNode = this.el;
+
+// Options for the observer (which mutations to observe)
+        const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+        const callback = (mutationList, observer) => {
+            for (const mutation of mutationList) {
+                if (mutation.type === "childList") {
+                    console.log("A child node has been added or removed.");
+                } else if (mutation.type === "attributes") {
+                    console.log(`The ${mutation.attributeName} attribute was modified.`);
+                }
+            }
+        };
+
+// Create an observer instance linked to the callback function
+        const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+        observer.observe(targetNode, config);*/
+
+// Later, you can stop observing
+       // observer.disconnect();
 
     }
 
